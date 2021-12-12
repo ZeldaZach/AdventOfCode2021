46a2447065885e0f449c29a17f34840d787d60f6
ZeldaZach <zahalpern+github@gmail.com>
2021-12-12 01:42:36 -0500

    Remove global variables as I was a dumb

--- entries/day12/main.py
+++ entries/day12/main.py
@@ -10,11 +10,6 @@ def read_inputs(input_file: str) -> List[List[str]]:
     return [[x.strip() for x in line.split("-")] for line in lines]
 
 
-# I don't like this, but I forget how to handle Graphs... will do some more studying
-part1_total_paths: int = 0
-part2_unique_paths: Set[Tuple[str, ...]] = set()
-
-
 def part1() -> int:
     # 49 minutes
     lines = read_inputs("input.txt")
@@ -24,18 +19,11 @@ def part1() -> int:
         graph[start_pos].add(end_pos)
         graph[end_pos].add(start_pos)
 
-    find_paths(graph, "start", "end", [])
-    return part1_total_paths
+    return find_paths(graph, "start", "end", [], set())
 
 
 def part2() -> int:
     # 10 minutes
-
-    # Reset globals
-    global part1_total_paths, part2_unique_paths
-    part1_total_paths = 0
-    part2_unique_paths = set()
-
     lines = read_inputs("input.txt")
 
     graph = defaultdict(set)
@@ -43,9 +31,10 @@ def part2() -> int:
         graph[start_pos].add(end_pos)
         graph[end_pos].add(start_pos)
 
+    part2_unique_paths = set()
     for double_entry in graph:
         if double_entry.islower():
-            find_paths(graph, "start", "end", [], double_entry)
+            find_paths(graph, "start", "end", [], part2_unique_paths, double_entry)
 
     return len(part2_unique_paths)
 
@@ -55,25 +44,41 @@ def find_paths(
     node: str,
     end_node: str,
     path: List[str],
+    unique_found_paths: Set[Tuple[str, ...]],
     allowed_twice: str = "",
-) -> None:
-    global part1_total_paths, part2_unique_paths
-
+) -> int:
+    """
+    DFS Find all paths from start to end
+    :param graph: Adjacency List Graph
+    :param node:  Current Node
+    :param end_node: Node to stop execution on
+    :param path: Current path taken
+    :param unique_found_paths: All uniquely found paths so far
+    :param allowed_twice: What lower case node, if any, can be found multiple times
+    :return: How many paths were found (pt1)
+    """
     if node == end_node:
-        part1_total_paths += 1
         if path.count("start") == 1 and end_node not in path:
-            part2_unique_paths.add(tuple(path))
-        return
+            unique_found_paths.add(tuple(path))
+        return 1
 
+    total_paths = 0
     for neighbor in graph[node]:
         if (
-            (neighbor not in path)
+            neighbor not in path
             or neighbor.isupper()
             or (neighbor == allowed_twice and path.count(allowed_twice) < 2)
         ):
-            find_paths(graph, neighbor, end_node, path + [node], allowed_twice)
+            total_paths += find_paths(
+                graph,
+                neighbor,
+                end_node,
+                path + [node],
+                unique_found_paths,
+                allowed_twice,
+            )
 
-    return
+    return total_paths
 
 
 def main() -> None:
